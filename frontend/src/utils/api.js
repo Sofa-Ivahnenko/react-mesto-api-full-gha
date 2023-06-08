658265ae12ed9094e1604f979eea4c6c13778ed8
Sofa Ivahnenko <109969162+Sofa-Ivahnenko@users.noreply.github.com>
2023-06-08 17:15:03 +0300

    deploy

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -124,7 +124,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: "https://api.websofa.mesto.nomoredomains.rock",
+  baseUrl: 'https://api.websofa.mesto.nomoredomains.rock',
 });
 
 export default api;
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://api.websofa.mesto.nomoredomains.rock";
+export const BASE_URL = 'https://api.websofa.mesto.nomoredomains.rock';
 
 function checkResponse(res) {
   if (res.ok) {
