378be48186d1f0f76b5446bd4dc7334235527c7a
Sofa Ivahnenko <109969162+Sofa-Ivahnenko@users.noreply.github.com>
2023-06-10 17:19:38 +0300

    al

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017g/mestodb ', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb ', {
   useNewUrlParser: true,
 });
 
