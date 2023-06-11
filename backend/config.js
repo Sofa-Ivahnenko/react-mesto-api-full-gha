4d996f3f886b95100ebe99996d55c047f2c8aa06
Sofa Ivahnenko <109969162+Sofa-Ivahnenko@users.noreply.github.com>
2023-06-12 00:40:33 +0300

    n

--- backend/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 3000,
-    MONGO_URL: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/mestodb',
-    JWT_SECRET: process.env.JWT_SECRET,
-    DEV_SECRET: 'dev-secret',
-    NODE_PRODUCTION: 'production',
-  };
-  
--- frontend/src/contexts/CurrentUserContext.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-export const currentUser = {};
-export const CurrentUserContext = React.createContext();
