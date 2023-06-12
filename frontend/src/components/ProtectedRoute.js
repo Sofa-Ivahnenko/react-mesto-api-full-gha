343371729d7904c58a10ac768099b358875957f0
Sofa Ivahnenko <109969162+Sofa-Ivahnenko@users.noreply.github.com>
2023-06-12 12:24:20 +0300

    jdk

--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ component: Component, ...props }) => {
-	return props.isLoggedIn ? <Component {...props} /> : <Navigate to="/signin" />;
-}
-
-export default ProtectedRoute;
