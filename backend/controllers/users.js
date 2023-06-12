f01e9eb5d51a88506e5a84a6f2e743d8a69489f3
Sofa Ivahnenko <109969162+Sofa-Ivahnenko@users.noreply.github.com>
2023-06-12 11:01:31 +0300

    add

---
+++ backend/controllers/users.js
@@ -0,0 +1,65 @@
+const userSchema = require('../models/user');
+const NotFoundError = require('../errors/not-found-error');
+
+module.exports.getUser = (req, res, next) => {
+  userSchema.findById(req.user._id)
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('User cannot be found');
+      } else {
+        res.send(user);
+      }
+    })
+    .catch(next);
+};
+
+module.exports.getAllUsers = (req, res, next) => {
+  userSchema.find({})
+    .then((users) => res.send(users))
+    .catch(next);
+};
+
+module.exports.getUserById = (req, res, next) => {
+  const { userId } = req.params;
+  userSchema.findById(userId)
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('User cannot be found');
+      } else {
+        res.send(user);
+      }
+    })
+    .catch(next);
+};
+
+module.exports.updateProfile = (req, res, next) => {
+  const { name, about } = req.body;
+  userSchema.findByIdAndUpdate(req.user._id, { name, about }, {
+    new: true,
+    runValidators: true,
+  })
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('User cannot be found');
+      } else {
+        res.send(user);
+      }
+    })
+    .catch(next);
+};
+
+module.exports.updateAvatar = (req, res, next) => {
+  const { avatar } = req.body;
+  userSchema.findByIdAndUpdate(req.user._id, { avatar }, {
+    new: true,
+    runValidators: true,
+  })
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('User cannot be found');
+      } else {
+        res.send(user);
+      }
+    })
+    .catch(next);
+};
