5b8603399aa8b5a82bf8162b0c85bbd2f2c46a52
Sofa Ivahnenko <109969162+Sofa-Ivahnenko@users.noreply.github.com>
2023-06-12 10:22:44 +0300

    fj

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -87,10 +87,10 @@ class Api{
 
 const api = new Api({
   baseUrl: 'https://api.websofa.mesto.nomoredomains.rocks',
-  headers: {
-		'Content-Type': 'application/json',
-		'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
-	},
+  // headers: {
+	// 	'Content-Type': 'application/json',
+	// 	'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
+	// },
   // headers:{
   //   authorization: '07d0cc49-29ca-4bb6-aef2-dd481f22cbcb',
   //   'Content-Type': 'application/json'
