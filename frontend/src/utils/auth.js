72f5da77088d46263c1782295d7dae4b1b739759
Sofa Ivahnenko <109969162+Sofa-Ivahnenko@users.noreply.github.com>
2023-06-13 09:56:29 +0300

    fjk

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -41,3 +41,43 @@
 //     }
 //   }).then(checkResponse);
 // }
+
+const checkResponse = (res) => {
+	if (res.ok) {
+		return res.json();
+	}
+	return Promise.reject(res.status);
+}
+
+const BASE_URL = 'https://api.websofa.mesto.nomoredomains.rocks';
+
+const signUp = (email, password) => {
+	const requestUrl = BASE_URL + '/signup';
+	return fetch(requestUrl, {
+		method: 'POST',
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ email, password }),
+	}).then(checkResponse);
+}
+
+const signIn = (email, password) => {
+	const requestUrl = BASE_URL + '/signin';
+	return fetch(requestUrl, {
+		method: 'POST',
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ email, password }),
+	}).then(checkResponse);
+}
+
+const checkToken = (token) => {
+	const requestUrl = BASE_URL + '/users/me';
+	return fetch(requestUrl, {
+		method: 'GET',
+		headers: {
+			"Content-Type": "application/json",
+			"Authorization": `Bearer ${token}`
+		},
+	}).then(checkResponse);
+}
+
+export { signUp, signIn, checkToken };
