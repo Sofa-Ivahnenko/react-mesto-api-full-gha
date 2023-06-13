a468c76d536181de0b823cfcc3732a4c54104853
Sofa Ivahnenko <109969162+Sofa-Ivahnenko@users.noreply.github.com>
2023-06-13 12:19:20 +0300

    fhjd

--- frontend/src/utils/apiAuth.js
+++ frontend/src/utils/apiAuth.js
@@ -9,24 +9,33 @@ const BASE_URL = 'https://api.websofa.mesto.nomoredomains.rocks';
 
 const signUp = (email, password) => {
 	const requestUrl = BASE_URL + '/signup';
+	const token = localStorage.getItem("jwt");
 	return fetch(requestUrl, {
 		method: 'POST',
-		headers: { "Content-Type": "application/json" },
+		headers: { 
+			"Content-Type": "application/json",
+			"Authorization": `Bearer ${token}`
+		 },
 		body: JSON.stringify({ email, password }),
 	}).then(checkResponse);
 }
 
 const signIn = (email, password) => {
 	const requestUrl = BASE_URL + '/signin';
+	const token = localStorage.getItem("jwt");
 	return fetch(requestUrl, {
 		method: 'POST',
-		headers: { "Content-Type": "application/json" },
+		headers: { 
+			"Content-Type": "application/json",
+			"Authorization": `Bearer ${token}` 
+		},
 		body: JSON.stringify({ email, password }),
 	}).then(checkResponse);
 }
 
 const checkToken = (token) => {
 	const requestUrl = BASE_URL + '/users/me';
+	const token = localStorage.getItem("jwt");
 	return fetch(requestUrl, {
 		method: 'GET',
 		headers: {
