dbbcfaed955871eb11058cada46489ff5946e8e0
Sofa Ivahnenko <109969162+Sofa-Ivahnenko@users.noreply.github.com>
2023-06-15 10:22:41 +0300

    fkd

---
+++ backend/app.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const cors = require('cors');
+const router = require('./routes/index');
+const { createUser, login } = require('./controllers/auth');
+const auth = require('./middlewares/auth');
+const { createUserValidation, loginValidation } = require('./middlewares/validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const {
+  MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb',
+  PORT = 3000,
+} = process.env;
+
+const app = express();
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server is about to crash');
+  }, 0);
+});
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  express.urlencoded({ extended: true }),
+);
+app.use(requestLogger);
+app.post('/signin', loginValidation, login);
+app.post('/signup', createUserValidation, createUser);
+app.use(auth);
+app.use(router);
+app.use(errorLogger);
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const {
+    status = 500,
+    message,
+  } = err;
+  res.status(status)
+    .send({
+      message: status === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
+
+async function start() {
+  try {
+    await mongoose.connect(MONGO_URL);
+    await app.listen(PORT);
+    await console.log(`App listening on port ${PORT}`);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+start();
---
+++ frontend/src/utils/auth.js
@@ -0,0 +1,42 @@
+// export const BASE_URL = "https://api.websofa.mesto.nomoredomains.rocks";
+
+// function checkResponse(res) {
+//   if (res.ok) {
+//     return res.json();
+//   }
+//   return Promise.reject(`Ошибка${res.status}`);
+// }
+
+// // Запрос для отправки данных на сервере
+// // При попытке авторизоваться (для компонента Логин)
+// export function authorization(email, password) {
+//   return fetch(`${BASE_URL}/signin`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ email, password }),
+//   }).then(checkResponse);
+// }
+
+// // Запрос для отправки данных на сервере
+// // При попытке зарегестрироваться (для компонента Регистер)
+// export function registration(email, password) {
+//   return fetch(`${BASE_URL}/signup`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ email, password }),
+//   }).then(checkResponse);
+// }
+
+// // Запрос на получение токена
+// export function getToken(token) {
+//   return fetch(`${BASE_URL}/users/me`, {
+//     headers: {
+//       "Content-Type": "application/json",
+//       authorization: `Bearer ${token}`
+//     }
+//   }).then(checkResponse);
+// }
